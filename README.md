d63586b0a38d1d113aa60693f5ba5d6f91024df8
nohjuhyeon <njh2720@gmail.com>
2024-01-03 17:48:18 +0900

    으악!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ```
 ~$ pip install fastapi jinja2
-
+ 
 ```
--- routes/users.py
+++ routes/users.py
@@ -7,8 +7,13 @@ from fastapi import Request
 # html 틀이 있는 폴더 위치
 templates = Jinja2Templates(directory="templates/")
 
-# 회원 가입
-@router.get("/inserts")
+# 회원 가입 from /users/form -> users/inserts.html
+@router.get("/form")
 async def home(request:Request):
     pass
     return templates.TemplateResponse(name="users/inserts.html",context = {"request": request})
+# 회원 가입 /users/inserts -> users/login.html
+@router.get("/inserts")
+async def home(request:Request):
+    pass
+    return templates.TemplateResponse(name="users/logins.html",context = {"request": request})
